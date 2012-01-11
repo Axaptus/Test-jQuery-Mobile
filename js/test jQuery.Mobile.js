29d006e75564c7a45dc60bc5abb7758558370c8d
unknown <Martin Campbell@Vanda.(none)>
2012-01-10 22:29:49 -0300

    From Aptana - 20120110

--- js/test jQuery.Mobile.js
+++ js/test jQuery.Mobile.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
 
+   	var toggle = true;
+       	
     $("#slLists").click(function() {
         $.mobile.changePage("#slListsPage", {changeHash:true, transition:"slide"});
         return false;
     });
 
-    // Initialize the list view
-    //$("#listHome").listview();
-    $("#slAddListItem").click(function() {
+    $("#slAddListItem1").click(function() {
 
-            var idText = Math.random();
+            //var idText = Math.random();
             // Initialize the list view
             //$("#listHome").listview();
 
-            $("#listHome").append("<li><a href='#' id='slExit'" + idText + ">Salir " + idText + "</a></li>").listview("refresh");
-    });
+            //$("#listHome").append("<li><a href='#' id='slExit'" + idText + ">Salir " + idText + "</a></li>").listview("refresh");
+            
+       	//var mySlider = $("#slider-music");
 
+		// Disable a slider
+		//mySlider.slider('disable');
+		// Enable a slider
+		//mySlider.slider("enable");
+		// Manually flip a slider and update its UI state
+		// (just flipping it will not automatically update the UI)
+		//mySlider[0].selectedIndex = !mySlider[0].selectedIndex;
+		//toggle = !toggle;
+		//mySlider.slider("refresh");
+		
+		$.mobile.silentScroll(500);
+    });
+    
+ 
 
 });
